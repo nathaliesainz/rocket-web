f325512fe8f312eccbfd40f7e9eaa024b41b9c6c
nathaliesainz <nathaliesainz13@gmail.com>
2023-06-21 13:11:56 -0600

    Add styles to body

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import HeaderRocket from './assets/rocket.svg'
+import ActiveRocket from './assets/rocketActive.svg'
+import DestroyedRocket from './assets/rocketDeactivate.svg'
+import UnknownRocket from './assets/rocketUnknown.svg'
 import './App.css'
 
 function App() {
@@ -9,19 +12,34 @@ function App() {
                 <div>
                   <img src={HeaderRocket} className='HeaderRocket' alt="Rocket Logo" />
                 </div>
-                <div className='boxLine'>
-                  <p className='mainLine'>Rocket</p>
-                  <p className='subLine'>Improving all the time</p>
-                  <a href="#" className='BtnStart'>Start</a>
+                <div className='boxTitles'>
+                  <p className='mainTitle'>Rocket</p>
+                  <p className='subtitle'>Improving all the time</p>
+                  <a href="#" className='btnStart'>Start</a>
                 </div>
             </header>
 
             <div>
             <h2>Capsules status:</h2>
+            <div className='rocketStatus'>
+              <div className='activeRockets'>
+                <span>Active</span>
+                <img src={ActiveRocket} alt="Active Rockets" />
+              </div>
+              <div className='destroyedRockets'>
+                <span>Destroyed</span>
+                <img src={DestroyedRocket} alt="Destroyed Rockets" />
+              </div>
+              <div className='unknownRockets'>
+                <span>Unknown</span>
+                <img src={UnknownRocket} alt="Unknown Rockets" />
+              </div>
+            </div>
             <h2>All capsules:</h2>
+              <div className='boxRockets'></div>
             </div>
 
-            <footer>
+            <footer className='footer'>
               <p>FIX YOUR LITTLE PROBLEM AND LIGHT THIS CANDLE.</p>
             </footer>
         </container>
